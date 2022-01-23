ce3d75debed81bf2bc1bc8d5884597be09fe6f97
Cylence <58346764+CylenceYT@users.noreply.github.com>
2022-01-23 12:45:29 -0500

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -6,7 +6,7 @@ Citizen.CreateThread(function()
       Citizen.Wait(0)
         if IsControlPressed(0, 249) then
           currentPos = GetEntityCoords(ped)
-          DrawMarker(27, currentPos.x, currentPos.y, currentPos.z - 0.9, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 255, 0, 0, 50, false, true, 2, nil, nil, false)
+          DrawMarker(27, currentPos.x, currentPos.y, currentPos.z - 0.9, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 255, 0, 0, 50, false, true, 2, nil, nil, true)
         end
     end
 end)
